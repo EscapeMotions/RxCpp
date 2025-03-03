bb4688a8d50e0222a9fb513d7e80944271aa42f0
Michal Fapso <michal.fapso@gmail.com>
2025-03-03 11:48:55 +0100

    define GCC_VERSION only when it's not defined yet

--- Rx/v2/src/rxcpp/rx-includes.hpp
+++ Rx/v2/src/rxcpp/rx-includes.hpp
@@ -52,9 +52,11 @@
 
 #elif defined(__GNUG__)
 
+#if !defined (GCC_VERSION)
 #define GCC_VERSION (__GNUC__ * 10000 + \
                      __GNUC_MINOR__ * 100 + \
                      __GNUC_PATCHLEVEL__)
+#endif
 
 #if GCC_VERSION >= 40801
 #define RXCPP_USE_RVALUEREF 1
